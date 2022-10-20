2aa6d1f3727908a00aaf5143808b536aa6fb019d
Luis Dourado <lfaddourado@gmail.com>
2022-10-20 01:15:53 -0300

    feat: add alt description to each icon

--- pages/Job.js
+++ pages/Job.js
@@ -12,11 +12,19 @@ const Job = ({ company_name, title, remote, url, location, created_at }) => {
         <div className="flex justify-between mt-4 ">
           <div className="">
             <h4 className="flex gap-2">
-              <img src="/briefcase-solid.svg" className="w-3" />
+              <img
+                src="/briefcase-solid.svg"
+                className="w-3"
+                alt="Icone de maleta indicando que o texto a seguir é o nome da empresa"
+              />
               {company_name}
             </h4>
             <h5 className="flex gap-2">
-              <img src="/location-dot-solid.svg" className="w-3" />{' '}
+              <img
+                src="/location-dot-solid.svg"
+                className="w-3"
+                alt="Icone de um waypoint indicando que o texto a seguir corresponde a localidade da vaga"
+              />{' '}
               {remote ? 'Remoto' : `${location}`}
             </h5>
           </div>
