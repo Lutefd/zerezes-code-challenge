0e152e13be605ec494e77fa4fad30f6b84ff73ce
Luis Dourado <lfaddourado@gmail.com>
2022-10-20 13:43:55 -0300

    Feat: add milisseconds converter constant

--- utils/constants.js
+++ utils/constants.js
@@ -2,5 +2,6 @@ export const Constants = () => {
   const elementsPerPage = 12;
   const timeoutValue = 500;
   const initialPageIndex = 0;
-  return { elementsPerPage, timeoutValue, initialPageIndex };
+  const toMilliseconds = 1000;
+  return { elementsPerPage, timeoutValue, initialPageIndex, toMilliseconds };
 };
